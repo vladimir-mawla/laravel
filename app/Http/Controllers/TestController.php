823923e8cea5dd60952cb4b75dd20837fd03420b
vladimir <vladimirmawla2@gmail.com>
2022-06-06 23:16:49 +0300

    Adding updated version of the functions

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class TestController extends Controller{
-    
+    //say hi function
     public function sayHi(){
         $message = "From DB";
 
@@ -13,6 +14,8 @@ class TestController extends Controller{
             "message" => $message
         ], 200);
     }
+
+    //palindrome function
     public function palindrome(){
         $a=['abc','aba','bac','bab','afd', 'asa'];
         $number=0;
@@ -23,34 +26,71 @@ class TestController extends Controller{
         };
         echo $number;
     }
+
+    //seconds function
     public function seconds(){
         $sec = time() + (31536000*238) - (105*864000);
         echo $sec;
     }
+
+    //nominate pablo function
     public function nominate(){
         $a=["Pablo","Pablo","Pablo","Pablo","Georgio","Nour","Vladimir","Stephanelle","Mohammad","JP","Pablo","Pablo","Pablo"];
         $nominated=rand(0,count($a)-1);
         echo $a[$nominated];
     }
-    public function group() { 
-        $a = ["Pablo", "Alaa", "Georgio", "Vladimir", "Mohamad", "Hsein", "Tarek", "Nour"];
-        $select1=0;
-        $select2=0;
-        $obj = (array)$obj;
-        while(true){
-            $select1 = rand(0, count($a)-1);
-            echo json_encode($a[$select1]);
-            unset($a[$select1]);
-            var_dump($a);
-
-            $select2 = rand(0, count($a)-1);
-            echo json_encode($a[$select2]);
-            unset($a[$select2]);
-            var_dump($a);
-            //$obj['team1:'] = "$select1", "$select2";
-        }
+    //create group of 2 students
+        public function teams() { 
+            $a = ["Pablo", "Alaa", "Georgio", "Vladimir", "Mohamad", "Hsein", "Tarek", "Nour"];
+            $select1=0;
+            $select2=0;
+            $team =[];
+            $sum = count($a);
+            while($select1 < $sum){
+                if ($select1 % 2 == 0) {
+                    $team[$select2] = [$a[$select1], $a[$select1+1]];
+                    $select1 +=2;
+                    $select2 +=1;
+                } else {
+                    $team[$select2] = [$a[$select1]];
+                }
+/*
+                $select1 = rand(0, count($a)-1);
+                echo json_encode($a[$select1]);
+                unset($a[$select1]);
+                var_dump($a);
+    
+                $select2 = rand(0, count($a)-1);
+                echo json_encode($a[$select2]);
+                unset($a[$select2]);
+                var_dump($a);
+*/
+               
+            }
+            return response()->json([
+                "result" => $team,], 200);
+            
     }
 
-
+    //get beer recipe
+    public function beers(){
+        $response = Http::get("https://api.punkapi.com/v2/beers");
+        $data = json_decode($response);
+        $select = rand(0,((int)$data)-1);
+        return response()->json([
+            "result" => $response[$select],
+        ], 200);
+        
+        
+    }
+    //get text
+    public function text(){
+        $response = Http::get('http://example.com');
+        $response_decoded = json_decode($response);
+        $text = $response_decoded -> attachments['text'];
+        return response()->json([
+            "result" => $text,
+        ],200);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,6 @@ Route::get('/bye', [TestController::class, 'sayBye'])->name("say-bye");
 Route::get('/palindrome', [TestController::class, 'palindrome'])->name("palindrome");
 Route::get('/seconds', [TestController::class, 'seconds'])->name("seconds");
 Route::get('/nominate', [TestController::class, 'nominate'])->name("nominate");
-Route::get('/group', [TestController::class, 'group'])->name("group");
+Route::get('/teams', [TestController::class, 'teams'])->name("teams");
+Route::get('/text', [TestController::class, 'text'])->name("text");
+Route::get('/beers', [TestController::class, 'beers'])->name("beers");
